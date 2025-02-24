ac65d1fee1a36cc580206aa03d21c0617b4ece40
Arsen Avetisyan <arsen_avetisyan_2005@mail.ru>
2025-02-24 19:02:58 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+menu.onclick = function myFunction() {
+    var x = document.getElementById('myTopnav');
+    
+    if (x.className === "container") {
+        x.className += " responsive";
+    } else {
+        x.className = "container";
+    }
+}
